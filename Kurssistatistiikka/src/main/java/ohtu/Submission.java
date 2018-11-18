e35039d8352b1130f49da3869eb7e400aa977eeb
hjoonas <heik.joonas@gmail.com>
2018-11-18 21:40:46 +0200

    Tehtävä 1

---
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -0,0 +1,47 @@
+package ohtu;
+
+import java.util.List;
+
+public class Submission {
+    private int week;
+    private int hours;
+    private List<Object> exercises;
+    private String course;
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public void setHours(int hour) {
+        this.hours = hour;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+    
+    public void setExercises(int exercise) {
+        this.exercises.add(exercise);
+    }
+
+    public List getExercises() {
+        return exercises;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    public String toString() {
+        return course+", viikko "+week+" tehtyjä tehtäviä yhteensä " + exercises.size()+" aikaa kului "+ hours+ " tehdyt tehtävät: "+exercises;
+    }
+    
+}
